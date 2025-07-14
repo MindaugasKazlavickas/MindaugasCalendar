0b7cc244ca8ffae6c491c5fde9cc736eef8ce520
Mindaugas Kazlavickas <mindaugask@wix.com>
2025-07-14 15:57:07 +0300

    Header left, header center improvements

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     let headerButton = "calendar active";
-    let currentDay = new Date().getDate();
+    let currentDay = new Date();
 
     const calendarSideView = document.getElementById('calendarSideView');
     const weekView = document.getElementById('weekView');
@@ -18,8 +18,20 @@ document.addEventListener('DOMContentLoaded', () => {
             headerButton = "calendar active";
         }
     })
-    const logoDate = document.getElementById('logoDate');
+    const logoDate = document.getElementById('logoText');
     
-    logoDate.innerText = currentDay;
-
+    logoDate.innerText = currentDay.getDate();
+    const monthDisplay = document.getElementById('monthDisplay');
+    monthDisplay.innerText = monthsLong[currentDay.getMonth()] + ", " + currentDay.getFullYear();
 })
+
+const monthsShort = [
+    "Jan", "Feb", "Mar", 
+    "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", 
+    "Oct", "Nov", "Dec"];
+const monthsLong = [
+    "January", "February", "March", 
+    "April", "May", "June",
+    "July", "August", "September", 
+    "October", "November", "December"];
