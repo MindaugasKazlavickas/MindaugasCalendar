22bfec3a38d243ca9877fa1b53f44674a23a63a6
Mindaugas Kazlavickas <mindaugask@wix.com>
2025-08-14 13:21:41 +0300

    correction for RightSideTrigger

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Header from "./components/headeris";
+import Header from "./components/Header";
 import Content from "./components/MainContent";
 import RightSideTrigger from "./components/RightSideTrigger";
-import Event from "./components/eventas";
+import Event from "./components/Event";
 function App() {
   return (
     <div className="App">
