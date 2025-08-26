860a155284baf5f08d4758b606ec99f5d3b4cd23
Mindaugas Kazlavickas <mindaugask@wix.com>
2025-08-26 17:24:46 +0300

    cleanup, fixed webkit and firefox random crashes

--- calendar-app/tests/eventFlow.spec.ts
+++ calendar-app/tests/eventFlow.spec.ts
@@ -1,6 +1,27 @@
 //@ts-check
 import { test, expect } from "@playwright/test";
 
+test.beforeEach(async ({ page }) => {
+  await page.addInitScript(`
+    //Mock Date.now()
+     const fixedNow = 1756197020781;
+     Date.now = () => fixedNow;
+     
+     //Mock new Date()
+     const OriginalDate = Date;
+     class MockDate extends OriginalDate {
+     constructor(...args) {
+     if (args.length === 0) {
+     super(fixedNow);
+     } else {
+        super(...args);}
+      }
+        }
+      MockDate.now = () => fixedNow;
+      MockDate.UTC = OriginalDate.UTC;
+      MockDate.parse = OriginalDate.parse;
+      window.Date = MockDate`);
+});
 test("saving, editing and deleting event user flow", async ({ page }) => {
   //jest.spyOn(global.Date, "now").mockReturnValue(1756197020781);
 
@@ -14,7 +35,7 @@ test("saving, editing and deleting event user flow", async ({ page }) => {
   await page.getByRole("textbox", { name: "Add title" }).fill("Team meeting");
   await page.getByTestId("startDate").fill("2025-08-30");
   await page.getByTestId("startTime").fill("10:00");
-  await page.getByTestId("endTime").fill("10:30");
+  await page.getByTestId("endTime").fill("11:00");
   await page.getByTestId("locationField").click();
   await page.getByTestId("locationField").fill("Town hall office");
   await page.getByTestId("guestsField").click();
@@ -26,12 +47,15 @@ test("saving, editing and deleting event user flow", async ({ page }) => {
   await page.getByTestId("eventSaveButton").click();
   await expect(page.getByRole("dialog")).not.toBeVisible();
   await expect(
-    page.getByRole("row", { name: "10:00 - 10:30 Team meeting" })
+    page.getByRole("row", { name: "10:00 - 11:00 Team meeting" })
   ).toBeVisible();
 
   //open existing event and edit it
-  await page.getByRole("cell", { name: "10:00 - 10:30 Team meeting" }).click();
-  await expect(page.getByRole("dialog")).toBeVisible();
+  await page
+    .getByRole("cell", { name: "10:00 - 11:00 Team meeting" })
+    .click({ force: true });
+  //await page.waitForSelector('[role="dialog"]', { state: "visible" });
+  expect(page.getByRole("dialog")).toBeVisible();
   await expect(page.getByRole("textbox", { name: "Add title" })).toHaveValue(
     "Team meeting"
   );
@@ -42,12 +66,12 @@ test("saving, editing and deleting event user flow", async ({ page }) => {
   await page.getByTestId("eventSaveButton").click();
   await expect(page.getByRole("dialog")).not.toBeVisible();
   await expect(
-    page.getByRole("row", { name: "10:00 - 10:30 Team meeting updated" })
+    page.getByRole("row", { name: "10:00 - 11:00 Team meeting updated" })
   ).toBeVisible();
 
   //open existing event and delete it
   await page
-    .getByRole("cell", { name: "10:00 - 10:30 Team meeting updated" })
+    .getByRole("cell", { name: "10:00 - 11:00 Team meeting updated" })
     .click();
   await expect(page.getByRole("dialog")).toBeVisible();
   await expect(page.getByRole("textbox", { name: "Add title" })).toHaveValue(
@@ -56,6 +80,6 @@ test("saving, editing and deleting event user flow", async ({ page }) => {
   await page.getByRole("button", { name: "Delete event" }).click();
   await expect(page.getByRole("dialog")).not.toBeVisible();
   await expect(
-    page.getByRole("row", { name: "10:00 - 10:30 Team meeting updated" })
+    page.getByRole("row", { name: "10:00 - 11:00 Team meeting updated" })
   ).not.toBeVisible();
 });
--- calendar-app/tests/viewDisplay.spec.ts
+++ calendar-app/tests/viewDisplay.spec.ts
@@ -1,6 +1,28 @@
 //@ts-check
 import { test, expect } from "@playwright/test";
 
+test.beforeEach(async ({ page }) => {
+  await page.addInitScript(`
+    //Mock Date.now()
+     const fixedNow = 1756197020781;
+     Date.now = () => fixedNow;
+     
+     //Mock new Date()
+     const OriginalDate = Date;
+     class MockDate extends OriginalDate {
+     constructor(...args) {
+     if (args.length === 0) {
+     super(fixedNow);
+     } else {
+        super(...args);}
+      }
+        }
+      MockDate.now = () => fixedNow;
+      MockDate.UTC = OriginalDate.UTC;
+      MockDate.parse = OriginalDate.parse;
+      window.Date = MockDate`);
+});
+
 test("trigger panels and compare them with screenshots", async ({ page }) => {
   //jest.spyOn(global.Date, "now").mockReturnValue(1756197020781);
 
